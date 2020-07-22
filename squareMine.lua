a13758b9062e2fcbdc60be663c2a8bf007c89d4a
Eric Jayjock <ejayjock@gmail.com>
2020-07-22 14:35:10 -0400

    Created test to make turn alternate turning direction.

--- squareMine.lua
+++ squareMine.lua
@@ -20,17 +20,36 @@ function clear()
   end
 end
 
+function isodd(numb)
+
+
+end
+
+turnRight=true
 for j=1,10 do
- for i=1,10 do
-clearUp()
-clear()
-turtle.digDown()
-turtle.forward()
- end
-turtle.turnRight()
-clearUp()
-clear()
-turtle.digDown()
-turtle.forward()
-turtle.turnRight()
+  for i=1,10 do
+    clearUp()
+    clear()
+    turtle.digDown()
+    turtle.forward()
+  end
+
+  if turnRight then
+    turtle.turnRight()
+  else
+    turtle.turnLeft()
+  end
+  
+  clearUp()
+  clear()
+  turtle.digDown()
+  turtle.forward()
+
+  if turnRight then
+    turtle.turnRight()
+    turnRight=false
+  else
+    turtle.turnLeft()
+    turnRight=true
+  end
 end
