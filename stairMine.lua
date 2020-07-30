2ad7099f1baced6697a7eea262430346c8bf5710
Eric Jayjock <ejayjock@gmail.com>
2020-07-30 08:49:51 -0400

    Updated stairmine to Lucia's advanced code, and copied over Iilia's Farm Turtle.

--- stairMine.lua
+++ stairMine.lua
@@ -6,10 +6,20 @@ Series of turtle commands:
   - Move down
   - dig down
   - move down
+
   - dig Forward
   - move forward ]]
 
+print("how deep?")
+local vDeep=tonumber(io.read())
 
+print("What slot is the fill material in?")
+local nSlot=tonumber(io.read())
+
+print("What slot are the torches in?")
+local tSlot=tonumber(io.read())
+
+-- Use command  	turtle.getItemDetail([slotNum]) to get name
 
 function clearUp()
   cont=true
@@ -22,7 +32,6 @@ function clearUp()
   end
 end
 
-
 function clear()
   cont=true
   while cont do
@@ -34,24 +43,89 @@ function clear()
   end
 end
 
--- Ask how deep the mine shoud be
-print("How deep?")
-nDeeep=tonumber(io.read())
+function FillDown(slot)
+  if not turtle.detectDown() then
+    turtle.select(slot)
+    turtle.placeDown()
+  end
+end
 
-print("Which Slot has filler material?")
-nSlot=tonumber(io.read())
+function FillLeft(slot)
+  turtle.turnLeft()
+  if not turtle.detect() then
+    turtle.select(slot)
+    turtle.place()
+  end
+  turtle.turnRight()
+end
 
-for i=1,nDeeep do
+function FillRight(slot)
+  turtle.turnRight()
+  if not turtle.detect() then
+    turtle.select(slot)
+    turtle.place()
+  end
+  turtle.turnLeft()
+end
+
+function placeTorch(torchSlot)
+  turtle.turnLeft()
+  clear()
+  turtle.forward()
+  if not turtle.detect() then
+    turtle.select(slot)
+    turtle.place()
+  end
+  turtle.back()
+  turtle.select(torchSlot)
+  turtle.place()
+  turtle.turnRight()
+end
+
+function FillRightWall(slot)
+  FillRight(slot)
+  turtle.up()
+  FillRight(slot)
+  turtle.up()
+  FillRight(slot)
+  turtle.up()
+  FillRight(slot)
+end
+
+function FillLeftWall(slot)
+    FillLeft(slot)
+    turtle.down()
+    FillLeft(slot)
+    turtle.down()
+    FillLeft(slot)
+    turtle.down()
+    FillLeft(slot)
+end
+
+function FillUp(slot)
+  if not turtle.detectUp() then
+    turtle.select(slot)
+    turtle.placeUp()
+  end
+end
+
+local tSpace=3
+for i=1,vDeep do
   clearUp()
   turtle.up()
   clearUp()
   turtle.down()
   turtle.digDown()
   turtle.down()
-  if not turtle.detectDown()  then
-    turtle.select(nSlot)
-    turtle.placeDown()
+  FillDown(nSlot)
+  FillRightWall(nSlot)
+  FillUp(nSlot)
+  FillLeftWall(nSlot)
+  if tSpace == 1 then
+    placeTorch(tSlot)
+    tSpace=3
   end
+  tSpace=tSpace-1
   clear()
   turtle.forward()
 end
